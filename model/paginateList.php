d39493e7797b4cce171c934c2b7edc05e7778999
fed8055 <fed8055@yandex.ru>
2020-09-09 16:58:10 +0700

    добавлена модель пагинатора, класс и вьюха подлежит допилу

--- model/paginateList.php
+++ model/paginateList.php
@@ -8,9 +8,18 @@
             $this->dbh = $tmp->dbh;//ну так удобней будет. надеюсь сработает.
         }
 
-        public function getListAll($table){
-            //получать просто нумерованный массив. в виде индекс - ид записи. отдавать в контроллер.
 
+        public function getPage($postCount, $table, $fields){
+            $q = "select count(id) from $table";
+            $sth = $this->dbh->prepare($q);
+            $sth->execute($sth);
+            $res = $sth->fetchall(PDO::FETCH_COLUMN);
+            $pageCount = $res[0];//количество страниц
+
+            $startIndex = ($_GET['page'] * $postCount) - $postCount;
+
+            $q = "select $fields from $table limit $startIndex, $postCount";
+            return $this->dbh->query($q);//получается массив с ассоциативными массивами, самое то для форича
         }
 
     }
